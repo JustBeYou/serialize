c86f463681dbd5d69a33c2e03d5786420f47d4e0
JustBeYou <mihailferaru2000@gmail.com>
2020-06-26 00:43:18 +0300

    Test struct field header creation

---
+++ field_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestNewFieldHeader(t *testing.T) {
+	_, err := NewFieldHeader(0xff)
+	if err != nil && err.Error() != "invalid field ID" {
+		t.Error("ID validation failed")
+	}
+	_, err = NewFieldHeader(0xe1)
+	if err != nil && err.Error() != "multiple size flags set" {
+		t.Error("size validation failed")
+	}
+	_, err = NewFieldHeader(0x41)
+	if err != nil && err.Error() != "size flag set for something that is not an array" {
+		t.Error("array flag validation failed")
+	}
+
+	h, err := NewFieldHeader(0xc1)
+	if err != nil || h.IsArray == false || h.Is16BitSize == false || h.TypeID != UintID {
+		t.Error("invalid header creation")
+	}
+}
+
+func TestFieldHeader_Serialize(t *testing.T) {
+	h := FieldHeader{
+		UintID,
+		true,
+		false,
+		false,
+		false,
+	}
+	output, err := h.Serialize()
+	if err != nil || output[0] != 0x81 {
+		t.Errorf("invalid serialized output: %x", output[0])
+	}
+}
