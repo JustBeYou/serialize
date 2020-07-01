6c621a8ca6e41243de5ab7765aa3f88216a7817d
JustBeYou <mihailferaru2000@gmail.com>
2020-07-01 17:47:45 +0300

    Samples

--- sample/foo.go
+++ sample/foo.go
@@ -1,10 +1,25 @@
 package sample
 
-//go:generate serialize -file=$GOFILE -type=Foo
+//go:generate serialize -file=$GOFILE -type=Foo -serializer=hashing
 type Foo struct {
 	Bar int
 	Fizz uint
 	Buzz string
 	FizzBuzz bool
 	BarArray []bool
+	Hash string `hashing:"ignore"`
+	Custom Bar
+}
+
+//go:generate serialize -file=$GOFILE -type=Bar -serializer=hashing
+type Bar struct {
+	Value int
+}
+
+type HashingSerializer interface {
+	HashingSerialize() ([]byte, error)
+}
+
+type HashingUnserializer interface {
+	HashingUnserialize([]byte) (interface{}, error)
 }
--- sample/foo_test.go
+++ sample/foo_test.go
@@ -12,23 +12,25 @@ func TestFoo(t *testing.T) {
 		"foobar",
 		true,
 		[]bool{true, false, true},
+		"AAAA",
+		Bar{100},
 	}
 
 	output, _ := foo.Serialize()
 
-	expected := []byte{246, 255, 255, 255, 255, 255, 255, 255, 20, 0, 0, 0, 0, 0, 0, 0, 0, 6, 102, 111, 111, 98, 97, 114, 1, 0, 3, 1, 0, 1}
+	expected := []byte{246, 255, 255, 255, 255, 255, 255, 255, 20, 0, 0, 0, 0, 0, 0, 0, 0, 6, 102, 111, 111, 98, 97, 114, 1, 0, 3, 1, 0, 1, 0, 4, 65, 65, 65, 65, 100, 0, 0, 0, 0, 0, 0, 0}
 	if !bytes.Equal(output, expected) {
 		t.Errorf("Bad serialization: %v != %v", output, expected)
 	}
 
-	unserializedData, err := Foo{}.Unserialize(output)
+	unserializedData, _, err := Foo{}.Unserialize(output)
 	newFoo := unserializedData.(Foo)
 
 	if err != nil {
 		t.Errorf("Could not unserialize: %s\n", err.Error())
 	}
 
-	if newFoo.Bar != -10 || newFoo.Fizz != 20 || newFoo.Buzz != "foobar" || newFoo.FizzBuzz != true || len(newFoo.BarArray) != 3 || newFoo.BarArray[0] != true || newFoo.BarArray[1] != false || newFoo.BarArray[2] != true {
+	if newFoo.Bar != -10 || newFoo.Fizz != 20 || newFoo.Buzz != "foobar" || newFoo.FizzBuzz != true || len(newFoo.BarArray) != 3 || newFoo.BarArray[0] != true || newFoo.BarArray[1] != false || newFoo.BarArray[2] != true || newFoo.Custom.Value != 100 {
 		t.Errorf("Bad deserialization: %v != %v\n", newFoo, foo)
 	}
 }
