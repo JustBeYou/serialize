4d96410934f10ab23f81cd4a6b63cdbacb47c773
JustBeYou <mihailferaru2000@gmail.com>
2020-07-10 01:02:28 +0300

    Boilerplate for type conversion. Must be automated

--- sample/table.ser.go
+++ sample/table.ser.go
@@ -1,1 +1,18 @@
 package sample
+
+var TypeIdTable = map[string]uint16{
+	"Foo": 1,
+	"Bar": 2,
+}
+
+var IdTypeTable map[uint16]func([]byte) (interface{}, uint64, error)
+func init() {
+	IdTypeTable = map[uint16]func([]byte) (interface{}, uint64, error) {
+		1: func (data []byte) (interface{}, uint64, error) {
+			return Foo{}.Unserialize(data)
+		},
+		2: func (data []byte) (interface{}, uint64, error) {
+			return Bar{}.Unserialize(data)
+		},
+	}
+}
